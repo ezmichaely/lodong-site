cb7a60e1d635920da8b6f75fd5fcd56f5abb7d6b
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-09-15 19:44:01 +0800

    update partner images

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en"
       className={
-        `${applesd.variable} ${inter.variable}
-        ${nanum.variable} ${kumbh.variable} 
-        ${roboto_slab.variable} ${jacques.variable}
+        ` ${applesd.variable} ${inter.variable}
+          ${nanum.variable} ${kumbh.variable}
+          ${roboto_slab.variable} ${jacques.variable}
         `} >
 
       <body>
