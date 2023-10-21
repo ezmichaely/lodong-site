ac712544ba1a3858402baa171c683656ea64f21a
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-10-21 19:23:45 +0800

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,27 +6,21 @@ import {
   roboto_slab,
   jacques
 } from '@/assets/fonts';
-import {
-  Header,
-  Footer
-} from '@/components';
+import { Header, Footer } from '@/components';
 
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en"
-      className={
-        ` ${applesd.variable} ${inter.variable}
+  const htmlClass = ` ${applesd.variable} ${inter.variable}
           ${nanum.variable} ${kumbh.variable}
-          ${roboto_slab.variable} ${jacques.variable}
-        `} >
+          ${roboto_slab.variable} ${jacques.variable}`
 
+  return (
+    <html lang="en" className={htmlClass}>
       <body>
         <Header />
         {children}
         <Footer />
       </body>
-
     </html>
   )
 }
--- src/utils/getHeaderBg.js
+++ src/utils/getHeaderBg.js
@@ -1,21 +1,4 @@
-// export function getHeaderBg(path, mediaQuery) {
-//   // const { pathname, media } = path;
-//   if (path === '/' || path === '/contact') { return 'bg-transparent'; }
-//   else if ((path === '/aboutus' || path === '/wework') && !mediaQuery) { return 'bg-black' }
-//   else if ((path === '/aboutus' || path === '/wework') && mediaQuery) { return 'bg-transparent' }
-//   else if ((path === '/service' || path === '/projects') && !mediaQuery) { return 'bg-black' }
-//   else if ((path === '/service' || path === '/projects') && mediaQuery) { return 'bg-white' }
-//   else if (path === '/projects/keflavik' || path === '/projects/aicity' || path === '/projects/washcube' ||
-//     path === '/projects/winlottery' || path === '/projects/caremanager' || path === '/projects/rugolf') {
-//     return 'bg-black';
-//   }
-// }
-
-
-
-
 export function getHeaderBg(path, mediaQuery) {
-
   switch (path) {
     case '/':
     case '/contact':
