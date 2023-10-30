722173237f8a150f1cbbed9aa224e84cca25122b
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-10-30 21:49:24 +0800

    update contract

--- src/app/contact/section/ContactForm.jsx
+++ src/app/contact/section/ContactForm.jsx
@@ -3,8 +3,14 @@ import styles from './contactForm.module.css'
 
 export default function ContactForm() {
   return (
-    <section>
+    <section className={styles.contactForm}>
+      <form action="">
+        <input type="text" className='' placeholder='이름 (소속) 입력하기' />
+        <input type="text" className='' placeholder='연락처 입력하기' />
+        <div>
 
+        </div>
+      </form>
     </section>
   )
 }
